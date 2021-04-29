41d470174c13b1aff4b9ffe86a68e549ef5aeda8
Francesco Sica <fsica171@gmail.com>
2021-04-29 17:43:02 +0200

    jsnack2

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- Jsnack 1 DATE-->
 <?php
     $partite = array(
         array(
@@ -32,9 +33,47 @@
         )
     );
 
-    foreach ($partite as  $partita) {
-        echo $partita['homeTeam'].' - '.$partita['teamAway'].'  '.
-        $partita['score_homeTeam']. ' | '.$partita['homeTeam_teamAway'];
-        echo "<br><br><br>";
-    }
+
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $age = (int) $_GET['age'];
+    $auth = "Inserisci la mail";
+
+
 ?>
+
+
+<!-- Jsnack 2 DATE-->
+
+<div style="display: flex; justify-content: space-around;">
+
+    <div style="background-color: cyan; color: black; width: 300px;">
+        <h1>Jsnack 1</h1>
+        <?php
+        foreach ($partite as  $partita) {
+            echo
+                $partita['homeTeam'].' - '.$partita['teamAway'].'  '.
+                $partita['score_homeTeam']. ' | '.$partita['homeTeam_teamAway'];
+            echo
+                "<br><br><br>";
+        }?>
+    </div>
+
+
+    <div style="background-color: cyan; color: black; width: 300px;">
+        <h1>Jsnack 2</h1>
+        <?php
+            if (strlen($name) > 3 &&
+                strpos($mail,".") > 0 &&
+                strpos($mail,"@") > 0 &&
+                is_int($age)){
+                $auth = "Accesso riuscito";
+                echo $auth;
+
+            }else {
+                $auth = "Accesso negato";
+                echo $auth;
+            }
+        ?>
+    </div>
+</div>
